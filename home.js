0ac09bdd8dd5fbba1bb32bb11b2fd35ed6bde8e1
mdasifhosen <mdasifhosen2028@gmail.com>
2026-04-19 22:03:12 +0600

    added a cash out section

--- home.js
+++ home.js
@@ -1,3 +1,8 @@
+// logout
+document.getElementById("logout-button").addEventListener('click', function () {
+    window.location.href='./index.html'
+})
+
 
 const validPin=1234
 document.getElementById("add-money-btn").addEventListener('click', function (e) {
@@ -11,8 +16,13 @@ document.getElementById("add-money-btn").addEventListener('click', function (e) 
     const pin = parseInt(document.getElementById("add-pin").value)
 
     const availableBalance = parseInt(
-      document.getElementById("available-balance").innerText,
+      document.getElementById("available-balance").innerText
     );
+
+    if (bank === "Bank") {
+      alert("Please select a bank");
+      return;
+    }
 
     if (accountNumber.length < 11) {
         alert("please provide valid account number")
@@ -29,3 +39,19 @@ document.getElementById("add-money-btn").addEventListener('click', function (e) 
     document.getElementById("available-balance").innerText =
       totalNewAvailableBalance;
 })
+
+
+
+// toggling feature
+
+document.getElementById("add-button").addEventListener('click', function () {
+    document.getElementById("cash-out-parent").style.display='none'
+    document.getElementById("add-money-parent").style.display = "block";
+})
+
+
+document.getElementById("cash-out-button")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "block";
+  });
